b004b1bb43156fa2fb66052621d5617d5d3cdbf3
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-03-10 09:29:09 -0600

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+//import statements
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./styles/index.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
