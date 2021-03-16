f2cbac7307ad21f5eac2829ef4fcf56ee7fb57a8
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-03-15 21:00:05 -0500

    Created and updated the app.js
    Updated the code and added the connection to the MongoDB Atlas using connect to cluster code.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+const mongoose = require("mongoose")
+app.use(require('./routes/router'))
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://cluster0.gdkck.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+{ useNewUrlParser: true, useUnifiedTopology: true }).then((res) =>{
+  app.listen(3000, function () {
+  return "Connected to Database"
+  
+})
+}).catch((e) => {
+ console.log(e,"--error")
+})
