eeb6c36934a196c39baf9f61a5cc84d5024adfed
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2021-03-15 20:40:45 -0500

    Update and clean the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,100 +1,35 @@
-// import * as randLoc from './location.js';
-import locationsArray from '../locations.js';
-import {isInsideCircle} from './location-circle.js';
+ import React, { Component } from "react";
+import "./styles/index.css";
 
-let locationElement = document.getElementById("bgrtwo");
-let locationElement2 = document.getElementById("bgrone");
+class Main extends Component {
+    render() {
+      return (
+          <html>
+              <body>
+                  <main>
+                      <article>
+                    <div className = "header" id = "header">
+                        <h1>Quest Hunt</h1>
+                        <br></br><br></br>
+                    </div>
+       <div className = "myTable" id = "myTable">
+       <div className = "myTable-row" id = "myTable-row">
+           <div id="colorSelector1"></div> 
+           <div id="colorSelector2"></div>
+       </div>
+   </div>
+</article>
+</main>
+<br></br>
+</body>
+<footer>
+<i id = "random" className="fa">&#xf09b;</i> 
+<a href="https://github.com/Rajshekar2641/fullstack-app">Source Code on Github</a>
+</footer>
+</html>
 
-
-
-window.addEventListener('load', main);
-
-locationElement.addEventListener('click', locationHandler);
-locationElement.addEventListener('touch', locationHandler);
-locationElement2.addEventListener('click', colorFunction1);
-locationElement2.addEventListener('touch', colorFunction1);
-
-function main() {
-    console.log('Page is fully loaded');
+);
 }
-
-
-
-function colorFunction1() {
-    document.getElementById("targetloc").innerHTML = "The Treasure is in the location ";
-    document.getElementById("lbl").innerHTML = targetLoc.Name;
-    document.getElementById("device-lat1").innerHTML = targetLoc.coordinates[0].latitude;
-    document.getElementById("device-long1").innerHTML = targetLoc.coordinates[0].longitude;
-    let utterance = new SpeechSynthesisUtterance(`The location where the treasure is ${targetLoc.Name}`);
-    speechSynthesis.speak(utterance);
-
-
 }
-
-
-async function getLocation() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    }).then(position => {
-        return position;
-    });
-}
-
-let currentlat, currentlon, loc, error = true;
-let targetLoc = locationsArray[Math.floor(Math.random()*locationsArray.length)];
-
-async function locationHandler() {
-    const locText = await getLocation();
-    // loc = 'Your current Location';
-    // document.getElementById("location").innerHTML = loc;
-    currentlat = locText.coords.latitude;
-    console.log(currentlat)
-    document.getElementById("device-lat").innerHTML = currentlat.toFixed(6);
-    currentlon = locText.coords.longitude;
-    console.log(currentlon)
-    document.getElementById("device-long").innerHTML = currentlon.toFixed(6);
-    
-    locationsArray.forEach(function (value) {
-        if (isInside(value.Latitude, value.Longitude)) {
-            document.getElementById("location").innerHTML = value.Name;
-            let utterance = new SpeechSynthesisUtterance("Congratulations!, You found location ${value.Name}");
-            speechSynthesis.speak(utterance);
-            error = false;
-        }
-    });
-
-    if(error) {
-
-        console.log("error is here")
-        document.getElementById("error-message").innerHTML = "Sorry,You're not near to the treasure";
-         let utterance = new SpeechSynthesisUtterance("Sorry,You're not near to the treasure");
-         speechSynthesis.speak(utterance);
-     } else {
-         document.getElementById("error-message").innerHTML = "";
-     }
-    
-    }
-    function isInside(questLat, questLon) {
-        let distance = distanceBetweenLocations(questLat, questLon);
-        console.log("distance: " + distance);
-        if (distance < 30) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-function distanceBetweenLocations(questLat, questLon) {
-    const R = 6371e3;
-    const φ1 = currentlat * Math.PI / 180;
-    const φ2 = questLat * Math.PI / 180;
-    const Δφ = (questLat - currentlat) * Math.PI / 180;
-    const Δλ = (questLon - currentlon) * Math.PI / 180;
-
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    const d = R * c;
-    return d; 
-}
+export default Main;
+  
