8fcb392dacd24b7e34011af8935dbe4db93cc83b
Puneeth159 <60018781+Puneeth159@users.noreply.github.com>
2021-03-18 19:22:07 -0500

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -7,5 +7,17 @@ router.get('/', (req, res) => {
     res.render('../public/views/index.ejs')
 })
 
+const locationController = require('../controllers/locationController')
+
+ router.get('/location/', locationController.findAll)
+
+ router.get('/location/:locationId', locationController.findOne)
+
+ router.post('/location/', locationController.create)
+
+ router.put('/location/:locationId', locationController.update)
+
+ router.delete('/location/:locationId', locationController.delete)
+
 
 module.exports = router;
